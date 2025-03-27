10d6ee15f938b2e4e2f4a7c6a14bff8cf98dfaf1
anishpatel691 <youtubanish@gmail.com>
2025-03-27 10:28:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://easylearn-wine.vercel.app/',
+  origin: 'https://easylearn-wine.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
